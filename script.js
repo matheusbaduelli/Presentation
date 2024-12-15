794acd8889a98be817315726ab899ef1508b1316
Matheus Rodrigues de Lima <mhs.djc@gmail.com>
2024-12-15 13:39:07 -0300

    Alter font size responsive 3

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ function typeText(text, elementId, delay = 1000) {
 const lista = `Python, JavaScript, Power BI, SQL HTML e CSS`;
 const texto = `Olá, meu nome é Matheus, tenho 27 anos e sou natural de São Paulo. Atualmente, sou Analista de Processos e estou cursando Análise e Desenvolvimento de Sistemas. Minha paixão por tecnologia começou em 2022, e desde então, venho me dedicando e evoluindo cada vez mais na área. Ao longo dessa jornada, consegui adquirir e aprimorar algumas habilidades que considero fundamentais para a minha formação profissional, tais como: ${lista}. Estou sempre em busca de novos conhecimentos e desafios, com o objetivo de me tornar um profissional cada vez mais capacitado e atualizado nas tecnologias que moldam o futuro.`;
 
-typeText("Matheus Dev.", "minhaDiv", 100);
+typeText("Matheus Dev", "minhaDiv", 100);
 // typeText(texto, "div2", 30);
 
 window.addEventListener("DOMContentLoaded", () => {
